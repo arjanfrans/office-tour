77fa52f677edae09c8b32f66ffc81064595fb629
Arjan Frans <arjanfrans.com@gmail.com>
2020-09-23 20:56:05 +0200

    Updates

---
+++ precache-manifest.7437f5df1851ddbf0c87e563a33bfbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87d446ad43073c5bf0edda43772bb0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92adee67981aa922546c",
+    "url": "/static/css/main.621de8c3.chunk.css"
+  },
+  {
+    "revision": "b6f952f58b5f88e44d23",
+    "url": "/static/js/2.553d947e.chunk.js"
+  },
+  {
+    "revision": "693c27ac8fc04359cd1c4033bad147ca",
+    "url": "/static/js/2.553d947e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92adee67981aa922546c",
+    "url": "/static/js/main.d0c6fbf3.chunk.js"
+  },
+  {
+    "revision": "031bf55e143ff85ae7ff",
+    "url": "/static/js/runtime-main.a9110b7d.js"
+  }
+]);
